1a29525db93bfad37b8b334f65c5b2d63f3e1cb9
NghiaPham <phamtrongnghia1105@gmail.com>
2024-08-14 23:53:38 +0700

    chore: Update assetPrefix and basePath in next.config.js

---
+++ components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as CpuIcon } from './cpu-svgrepo-com.svg';
+export { default as DatabaseIcon } from './database-svgrepo-com.svg';
+export { default as InternetIcon } from './internet-svgrepo-com.svg';
+export { default as SecuritySafeIcon } from './security-safe-svgrepo-com.svg';
+import 'react';
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>AWS Workshops</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Benlab" />
+      <meta property="og:description" content="Let's learn together" />
+      <link rel="icon" type="image/png" href="/images/logo.png" />
+    </>
+  ),
+  logo: (
+    <span>
+      <Image
+        className="rounded-full"
+        src="/images/logo.png"
+        alt="logo"
+        height={50}
+        width={50}
+        style={{ borderRadius: "50%", display: "inline-block" }}
+      />{" "}
+      <b>BENLAB'S WORKSHOPS</b>
+    </span>
+  ),
   project: {
     link: "https://github.com/phamtrongngh/aws-workshops",
   },
   docsRepositoryBase: "https://github.com/phamtrongngh/aws-workshops",
   footer: {
     text: "",
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: "Give feedback",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Benlab's Workshops",
+    };
   },
 };
 
